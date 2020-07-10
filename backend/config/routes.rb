b95c29f5bfb5ecd4449db4793d84da37f580be30
ccedacero <cristian.cedacero@gmail.com>
2020-07-10 11:03:12 -0400

    update routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :food_orders
-  resources :foods
-  resources :menus
-  resources :orders
-  resources :kitchens
-  resources :users
+  # resources :foods
+  # resources :menus
+  resources :orders, only: [:show,:create, :update, :destroy]
+  resources :kitchens, only: [:index]
+  resources :users, only: [:create]    
+  resources :food_orders, only: [:index, :show,:update, :delete, :create]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
